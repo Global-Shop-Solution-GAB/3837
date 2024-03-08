b25709169604d07c44027501192eab79e62b45f7
Service Web <sw@gssmail.com>
2024-03-08 16:42:12 -0600

    Commit changes in C:\Websites\Git\emarlow\3837\ATG_3837_BI_Emp_Pnts.sql

---
+++ ATG_3837_BI_Emp_Pnts.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "ATG_3837_BI_Emp_Pnts"(
+ "Terminal" CHAR(3),
+ "Employee" CHAR(5),
+ "Name" CHAR(30),
+ "Date_In" DATETIME,
+ "Date_out" DATETIME,
+ "Shift" INTEGER,
+ "Dept" CHAR(4),
+ "Bal_Group" CHAR(8),
+ "Occ_ID" INTEGER,
+ "Occ_Name" CHAR(50),
+ "Occ_Point" DOUBLE,
+ "Occ_Date" DATE
+);
